db8516579ef5f61a8f7d1ea29194be62e9248a56
joaolucas_oh <joaolucas_oh@outlook.com>
2024-07-05 23:26:00 -0300

    Feature: Adding the product name on the input search and submiting the search

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import { homePageEl, secondPageEl } from '../support/pages/elements';
+describe('As a Customer, we want to see if the second item from the second results page has at least 1 item to be bought', () => {
+  it('search the Instax mini and get the second product from the second page', () => {
+    // Fill the text on the search input field
+    cy.fillField(homePageEl.inputSearch,'instax mini');  
+    cy.get(homePageEl.submitSearch).click();
   })
 })
